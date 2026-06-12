78b4dee5d7e9ab16ad18ff5744978c8ecfa4f597
Manvi Singh <manvisingh98302@gmail.com>
2026-06-13 02:38:23 +0530

    made website mobile responsive

--- manvi.js
+++ manvi.js
@@ -24,3 +24,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+function toggleMenu() {
+    var menu = document.getElementById("mobileMenu");
+    
+    
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
